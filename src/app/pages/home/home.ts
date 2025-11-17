55c0f1c1f52ffdaec60efaadf2a5ef6e0f13ecdc
soulittude <79145776+Soulittude@users.noreply.github.com>
2025-11-18 02:59:08 +0300

    home page done + bugfix for movie card

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MovieCard } from '../../components/movie-card/movie-card';
+import { MovieService } from '../../services/movie';
+import { Movie } from '../../interfaces/movie.interface';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MovieCard],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+  trendingMovies: Movie[] = [];
 
+  constructor(private movieService: MovieService) {}
+
+  ngOnInit(): void {
+    this.movieService.getTrendingMovies().subscribe((response) => {
+      this.trendingMovies = response.results;
+    });
+  }
 }
