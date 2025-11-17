63688c9754ceb6fc118d7824a9bd5d212e5690c8
soulittude <79145776+Soulittude@users.noreply.github.com>
2025-11-18 01:41:16 +0300

    Hi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
+import { SearchResults } from './pages/search-results/search-results';
+import { Home } from './pages/home/home';
+import { MovieDetails } from './pages/movie-details/movie-details';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'search/:query', component: SearchResults },
+  { path: 'movie/:id', component: MovieDetails },
+  { path: '**', redirectTo: '' },
+];
