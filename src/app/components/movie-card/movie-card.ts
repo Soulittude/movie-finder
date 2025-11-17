0c5a4815355c36cbe5f1d7b36323738c7e394d5b
soulittude <79145776+Soulittude@users.noreply.github.com>
2025-11-18 02:47:35 +0300

    Movie card component done

--- src/app/components/movie-card/movie-card.ts
+++ src/app/components/movie-card/movie-card.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Movie } from '../../interfaces/movie.interface';
 
 @Component({
   selector: 'app-movie-card',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './movie-card.html',
   styleUrl: './movie-card.css',
 })
 export class MovieCard {
+  @Input() movie!: Movie;
 
+  get posterUrl(): string {
+    if (this.movie.poster_path) {
+      return `https://image.tmdb.org/t/p/w500${this.movie.poster_path}`;
+    }
+
+    return `https://via.placeholder.com/500x750?text=No+Image`;
+  }
 }
