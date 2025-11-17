4272de73f4af1e0073537d6b4e77973e38b8f866
soulittude <79145776+Soulittude@users.noreply.github.com>
2025-11-18 02:37:49 +0300

    header done

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, FormsModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+  constructor(private router: Router) {}
 
+  onSearchSubmit(query: string) {
+    if (query) {
+      this.router.navigate(['search/', query]);
+    }
+  }
 }
